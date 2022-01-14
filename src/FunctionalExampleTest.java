c1966299383cbbdc0e46be2ebe0735b48fc7f34e
Ram <mbramakrishna12@gmail.com>
2022-01-14 19:41:12 +0530

    second commit

---
+++ src/FunctionalExampleTest.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.List;
+
+@FunctionalInterface
+public interface FunctionalExampleTest {
+	
+	void add(int a, int b);
+		
+	
+	
+	
+
+}
+
+
+
+class test{
+	
+	public static void main(String[] args) {
+		
+		
+		1,2[2,4],5,6
+		
+		
+	List<Integer> list=	Arrays.asList(1,2,[3,4],5,6);
+		
+		list.stream().map(e->e).forEach(System.out::println);
+		
+	
+	}	
+}
+	
+	
+	            
+	
+	           
+
+	
+	
+
+
+
+
+
